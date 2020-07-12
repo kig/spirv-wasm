6fabbd2a90cab291e0fd60d657f6a8cc13e63694
Ilmari Heikkinen <hei@heichen.hk>
2020-07-12 20:46:44 +0800

    fix cat

--- string/cat.glsl
+++ string/cat.glsl
@@ -2,6 +2,7 @@
 
 ThreadLocalCount = 32;
 ThreadGroupCount = 1;
+
 HeapSize = 65536;
 FromIOSize = 65536;
 ToIOSize = 65536;
@@ -12,28 +13,32 @@ shared int eof;
 
 void main() {
 
-    int64_t blockSize = 65536;
-
     int argc = arrLen(argv);
     for (int i = 1; i < argc; i++) {
         eof = 0;
         string filename = aGet(argv, i);
+        int64_t blockSize = int64_t(HeapSize - (strLen(filename)+31)/32*32);
         int64_t block = 0;
         barrier();
         while (eof == 0) {
             FREE(FREE_IO(
                 int64_t off = (block * int64_t(ThreadCount) + int64_t(ThreadId)) * blockSize;
+
                 io r = read(filename, off, size_t(blockSize), malloc(size_t(blockSize)));
                 barrier();
+
                 string res = awaitIO(r);
+                barrier();
+
                 for (int i = 0; i < ThreadCount; i++) {
                     barrier();
                     if (i == ThreadId && strLen(res) > 0) {
-                        FREE_IO(FREE(writeSync(stderr, concat(str(off), ": ", str(ioCount), "\n"))));
                         print(res);
                     }
                 }
+
                 if (strLen(res) < size_t(blockSize)) atomicAdd(eof, 1);
+
                 block++;
                 barrier();
             ))
