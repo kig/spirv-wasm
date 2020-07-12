4a8d495e0f9db276319859e4f4496d74412ec838
Ilmari Heikkinen <hei@heichen.hk>
2020-07-12 21:47:35 +0800

    fix cat better

--- string/cat.glsl
+++ string/cat.glsl
@@ -13,35 +13,40 @@ shared int eof;
 
 void main() {
 
+    int64_t blockSize = int64_t(HeapSize);
+
     int argc = arrLen(argv);
     for (int i = 1; i < argc; i++) {
         eof = 0;
         string filename = aGet(argv, i);
-        int64_t blockSize = int64_t(HeapSize - (strLen(filename)+31)/32*32);
         int64_t block = 0;
         barrier();
         while (eof == 0) {
-            FREE(FREE_IO(
-                int64_t off = (block * int64_t(ThreadCount) + int64_t(ThreadId)) * blockSize;
+            FREE(
+                string res;
+                FREE_IO(
+                    int64_t off = (block * int64_t(ThreadCount) + int64_t(ThreadId)) * blockSize;
 
-                io r = read(filename, off, size_t(blockSize), malloc(size_t(blockSize)));
-                barrier();
-
-                string res = awaitIO(r);
-                barrier();
-
-                for (int i = 0; i < ThreadCount; i++) {
+                    io r = read(filename, off, size_t(blockSize), malloc(size_t(blockSize)));
                     barrier();
-                    if (i == ThreadId && strLen(res) > 0) {
-                        print(res);
+
+                    res = awaitIO(r);
+                    barrier();
+                )
+                FREE_IO(
+                    for (int i = 0; i < ThreadCount; i++) {
+                        barrier();
+                        if (i == ThreadId && strLen(res) > 0) {
+                            print(res);
+                        }
                     }
-                }
 
-                if (strLen(res) < size_t(blockSize)) atomicAdd(eof, 1);
+                    if (strLen(res) < size_t(blockSize)) atomicAdd(eof, 1);
 
-                block++;
-                barrier();
-            ))
+                    block++;
+                    barrier();
+                )
+            )
         }
     }
 
